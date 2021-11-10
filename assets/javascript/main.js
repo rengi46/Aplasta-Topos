81e8bd945e3aa634ef205aa80d56309218ff2fff
Erick Ayala <erickayalrias@gmail.com>
2021-11-10 17:43:03 +0100

    topomalo

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -13,7 +13,7 @@ function abrirModal(){
 function start() {
     startTime = new Date
     closeModal()
-    Dificultad(score)
+    Dificultad(0)
     abrirModal()
 }
 
@@ -32,7 +32,7 @@ function TimeBar() {
         yourloose('Your time Finished')
         modalContent[0].appendChild(newP(`Your Final Score is : ${calculateScore()} seconds`))
         resetGame()
-        Dificultad(score)
+        Dificultad(1)
         var Despues = new Date
         // console.log(Despues.getTime() - antes.getTime())
     }, 20000)
@@ -40,6 +40,7 @@ function TimeBar() {
 
 //TODO stop Time Bar
 function stopTimeBar() {
+    console.log("1a")
     clearTimeout(gameTime)
     Tprogres.classList.remove("timeOut")
 }
@@ -48,6 +49,7 @@ function stopTimeBar() {
 function Dificultad(nivel=0, time = 50, max = 1000) {
     const d = max - (nivel*time)
     moveSpeed = setInterval(()=>{
+        console.log(nivel)
         añadiendolo()}, d)
 }
 
@@ -82,6 +84,19 @@ function topo(a)
         a.classList.add("oso")
     },200)
 }
+function casco(a){
+    a.classList.add("cascoBajando")
+    setTimeout(()=>{
+        a.classList.remove("cascoBajando")
+        a.classList.add("cascoOso")
+        // a.classList.add("oso")
+    },200)
+}
+function bomb(a){
+    const d = 1000 - (score*50)
+    a.classList.add("bomb")
+    setTimeout(()=>{a.classList.remove("bomb")},d)
+}
 function esconder(a){
     Cuadrado.forEach(cuadrado => {
         cuadrado.classList.remove("ososal")
@@ -95,23 +110,17 @@ function esconder(a){
     })
 }
 
-function bomb(a){
-    const d = 1000 - (score*50)
-    a.classList.add("bomb")
-    setTimeout(()=>{a.classList.remove("bomb")},d)
-}
-
 //TODO click events
 Cuadrado.forEach(cuadrado => {
     cuadrado.addEventListener("click", () => {
         if (cuadrado.classList[1] == "oso") {
             aplastaoso(cuadrado)
+        }else if (cuadrado.classList[1] == "cascoOso"){
+            aplastaOso(cuadrado)
         }
         else if(cuadrado.classList[1] == "bomb"){
             aplastabomba(cuadrado)
-        
-            //console.log(score)
-        } 
+        }
         else if (life > 1) {
             life--
             lives(life)
@@ -119,7 +128,6 @@ Cuadrado.forEach(cuadrado => {
             cleanModal()
             yourloose("You Don't have move Lives")
             resetGame()
-            Dificultad(score)
             modalContent[0].appendChild(newP(`Your Final Score is : ${calculateScore()} seconds`))
         }
     });
@@ -129,28 +137,23 @@ Cuadrado.forEach(cuadrado => {
 function aplastaoso(a){
     score++
     userScore.innerHTML = score
+    console.log("2a")
     clearInterval(moveSpeed)
     stopTimeBar()
     a.classList.add("osoaplastado")
     setTimeout(()=>{
         a.classList.remove("osoaplastado")
-        Dificultad(score)
+        Dificultad(3)
         cleanModal()
         yourTime(finalTime() / 1000)
         playerScore()
     },1000)
 }
 
-function casco(a){
-    a.classList.add("cascoBajando")
-    setTimeout(()=>{
-        a.classList.remove("cascoBajando")
-        a.classList.add("cascoOso")
-        // a.classList.add("oso")
-    },200)
-}
+
 function aplastabomba(a){
     a.classList.remove("bomb")
+    console.log("3a")
     clearInterval(moveSpeed)
     stopTimeBar()
     a.classList.add("explo")
@@ -160,6 +163,12 @@ function aplastabomba(a){
         yourloose("Te exploto en toda la cara")
         resetGame()
     },1000)
+}
+
+function aplastaOso(a){
+    console.log("pepe")
+    a.classList.remove("cascoOso")
+    a.classList.add("oso")
 }
 
 
@@ -178,6 +187,7 @@ function resetGame() {
     score = 0
     userScore.innerHTML = score
     scoreResult = 0
+    console.log("4a")
     clearInterval(moveSpeed)
 }
 
