fb897f1772059e395c7075e2a5f8741b6f00c9b3
Roger <rogerpuigdemasa@gmail.com>
2023-12-11 09:30:26 +0100

    update fetch

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -66,10 +66,18 @@ async function TimeBar() {
 }
 
 async function getMaxScore(){
-    const dataJson = await  fetch('http://localhost:1337/api/juegos')
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("Authorization", "Bearer fed5a6a3c56289b7bf9bd27f7f720ee593ae153bfed739bfc5a12a720202c66582833baae082fd5591bda5dc40015f31ce1a13c8e7d40cc0a879cdcd23566337e207d42805bd1d6897c603c1bdc989db7d6d034d5827d40619222969258a9445765db2417af1f8d31b7ac7b8a323a4a33af660f56f372a9d5f7d5f1a463f7501");
+    const dataJson = await  fetch('https://celebrated-friends-2382d9c517.strapiapp.com/api/juegos',
+    {
+        method: 'GET',
+        headers: myHeaders,
+    })
     const data = await dataJson.json()
+    console.log(data);
     const maxScore = data.data.find((element) => {
-        if(element.attributes.Juego == "topos"){
+        if(element.attributes.Juego == "Topos"){
             return element.attributes.UmbraPremio
         }
     })
